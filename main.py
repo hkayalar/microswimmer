b30aae305e98074626556dd8e1ec081f57f75b2f
hkayalar <69360142+hkayalar@users.noreply.github.com>
2023-06-01 12:13:18 +0300

    .

--- main.py
+++ main.py
@@ -4,49 +4,22 @@ import math
 import random
 
 class MagneticCoil:
-    def __init__(self):
+    def __init__(self, position):
         self.current = 2.0  # Amperes
         self.wire_radius = 0.0003  # meters
         self.wire_length = 9.0  # meters
         self.num_wraps = 214
         self.coil_radius = 0.05  # meters
         self.max_force = 20.0  # Newtons
+        self.position = position
 
     def get_magnetic_force(self, position, velocity):
-        force = np.zeros(3)
-        magnetic_field = self.calculate_magnetic_field(position)
-        gradient = self.calculate_magnetic_gradient(position)
-        force = np.cross(velocity, gradient) * self.current
-        if np.linalg.norm(force) > self.max_force:
-            force = force / np.linalg.norm(force) * self.max_force
-        return force
+        # Calculate the magnetic force between the coil and the player
+        distance = np.linalg.norm(position - self.position)
+        direction = (self.position - position) / distance
+        magnetic_force = direction * distance  # Adjust this calculation based on your requirements
+        return magnetic_force
 
-    def calculate_magnetic_field(self, position):
-        magnetic_field = np.zeros(3)
-        mu0 = math.pi * 4e-7  # Vacuum permeability
-        for i in range(self.num_wraps):
-            theta = i / self.num_wraps * 2 * math.pi
-            wire_position = np.array([
-                math.cos(theta) * self.coil_radius,
-                math.sin(theta) * self.coil_radius,
-                0.0
-            ])
-            wire_direction = position - wire_position
-            r = np.linalg.norm(wire_direction)
-            dB = mu0 * self.current / (4 * math.pi * r**3) * np.cross(wire_direction, np.array([0, 1, 0]))
-            magnetic_field += dB
-        return magnetic_field
-
-    def calculate_magnetic_gradient(self, position):
-        gradient = np.zeros(3)
-        delta = 1e-6
-        for i in range(3):
-            delta_position = np.zeros(3)
-            delta_position[i] = delta
-            magnetic_field1 = self.calculate_magnetic_field(position - delta_position)
-            magnetic_field2 = self.calculate_magnetic_field(position + delta_position)
-            gradient[i] = (magnetic_field2[i] - magnetic_field1[i]) / (2 * delta)
-        return gradient
 
 class Player:
     def __init__(self, max_speed=4.5):
@@ -57,21 +30,30 @@ class Player:
         self.rb = None
         self.magnetic_force = None
         self.coils = None
-        self.is_moving = False
+        self.is_attached = False
+        self.attachment_coil = None
 
     def start(self, rb, coils):
         self.rb = rb
         self.coils = coils
 
     def move(self, direction):
-        desired_velocity = direction * self.max_speed
-        velocity_change = desired_velocity - self.rb.velocity
-        self.rb.add_force(velocity_change, force_mode="VelocityChange")
+        if self.rb is not None:
+            desired_velocity = direction * self.max_speed
+            velocity_change = desired_velocity - self.rb.velocity
+            self.rb.add_force(velocity_change, force_mode="VelocityChange")
+
+    def attach(self, coil):
+        self.attachment_coil = coil
+        self.is_attached = True
 
     def update(self, position, velocity):
-        self.magnetic_force = np.zeros(3)
-        for coil in self.coils:
-            self.magnetic_force += coil.get_magnetic_force(position, velocity)
+        if self.is_attached and self.attachment_coil is not None:
+            self.magnetic_force = self.attachment_coil.get_magnetic_force(position, velocity)
+        else:
+            self.magnetic_force = np.zeros(3)
+            for coil in self.coils:
+                self.magnetic_force += coil.get_magnetic_force(position, velocity)
 
         speed = np.linalg.norm(velocity)
         drag_force = self.drag_coefficient * self.fluid_viscosity * np.pi * self.hydro_radius * speed
@@ -81,29 +63,51 @@ class Player:
         if speed > self.max_speed:
             self.rb.velocity = self.rb.velocity / np.linalg.norm(self.rb.velocity) * self.max_speed
 
-class ObjectDetector:
-    def __init__(self):
-        self.net = None
 
-    def load_model(self, model_path, config_path):
-        self.net = cv2.dnn.readNetFromTensorflow(model_path, config_path)
+class ColorTracker:
+    def __init__(self, lower_color, upper_color):
+        self.lower_color = np.array(lower_color, dtype=np.uint8)
+        self.upper_color = np.array(upper_color, dtype=np.uint8)
+        self.tracked_object = None
+        self.tracking_threshold = 0.5
 
-    def detect_objects(self, frame):
-        blob = cv2.dnn.blobFromImage(frame, size=(300, 300), swapRB=True, crop=False)
-        self.net.setInput(blob)
-        detections = self.net.forward()
-        return detections
+    def track_color(self, frame):
+        hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
+        mask = cv2.inRange(hsv, self.lower_color, self.upper_color)
+        contours, _ = cv2.findContours(mask, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+
+        if len(contours) > 0:
+            largest_contour = max(contours, key=cv2.contourArea)
+            x, y, w, h = cv2.boundingRect(largest_contour)
+            detection = np.array([[x / frame.shape[1], y / frame.shape[0], (x + w) / frame.shape[1], (y + h) / frame.shape[0]]])
+            confidence = cv2.contourArea(largest_contour) / (frame.shape[0] * frame.shape[1])
+        else:
+            detection = np.empty((0, 4))
+            confidence = 0.0
+
+        if confidence > self.tracking_threshold:
+            self.tracked_object = detection
+
+        return detection
+
 
 def main():
-    # Initialize magnetic coil and player
-    magnetic_coil = MagneticCoil()
+    # Initialize magnetic coils and player
+    coil_positions = [
+        [0.1, 0.1, 0.0],  # Specify the positions of the coils in the pool (x, y, z) in camera's perspective
+        [0.2, 0.2, 0.0],
+        [0.3, 0.3, 0.0],
+        [0.4, 0.4, 0.0],
+        [0.5, 0.5, 0.0],
+        [0.6, 0.6, 0.0],
+        [0.7, 0.7, 0.0],
+        [0.8, 0.8, 0.0]
+    ]
+    coils = [MagneticCoil(position) for position in coil_positions]
     player = Player()
 
-    # Load object detection model
-    object_detector = ObjectDetector()
-    model_path = "path_to_model/frozen_inference_graph.pb"
-    config_path = "path_to_model/ssd_mobilenet_v2_coco.pbtxt"
-    object_detector.load_model(model_path, config_path)
+    # Set up color tracker
+    color_tracker = ColorTracker([0, 0, 0], [50, 50, 50])  # Adjust the lower and upper color values as per your requirement
 
     # Set up camera
     camera = cv2.VideoCapture(0)
@@ -112,25 +116,30 @@ def main():
         # Read frame from camera
         ret, frame = camera.read()
 
-        # Detect objects in the frame
-        detections = object_detector.detect_objects(frame)
+        # Track color in the frame
+        detection = color_tracker.track_color(frame)
 
-        # Process detections
-        for i in range(detections.shape[2]):
-            confidence = detections[0, 0, i, 2]
-            if confidence > 0.5:
-                class_id = int(detections[0, 0, i, 1])
-                if class_id == 1:  # Assuming class_id 1 corresponds to the player object
-                    x = int(detections[0, 0, i, 3] * frame.shape[1])
-                    y = int(detections[0, 0, i, 4] * frame.shape[0])
-                    cv2.circle(frame, (x, y), 5, (0, 255, 0), -1)
-                    # Control player's movement based on detected position
-                    player.move(np.array([(x - frame.shape[1] / 2) / (frame.shape[1] / 2),
-                                          (y - frame.shape[0] / 2) / (frame.shape[0] / 2),
-                                          0.0]))
+        # Process detection
+        if len(detection) > 0:
+            x, y, _, _ = detection[0]
+            x = int(x * frame.shape[1])
+            y = int(y * frame.shape[0])
+            cv2.circle(frame, (x, y), 5, (0, 255, 0), -1)
+
+            # Control player's movement based on detected position
+            player.move(np.array([(x - frame.shape[1] / 2) / (frame.shape[1] / 2),
+                                  (y - frame.shape[0] / 2) / (frame.shape[0] / 2),
+                                  0.0]))
+
+            # Attach player to a coil if not already attached
+            if not player.is_attached:
+                for coil in coils:
+                    if np.array_equal(coil.position, [x, y, 0.0]):
+                        player.attach(coil)
+                        break
 
         # Show frame with detections
-        cv2.imshow("Object Detection", frame)
+        cv2.imshow("Color Tracking", frame)
 
         # Check for exit key
         if cv2.waitKey(1) == ord('q'):
@@ -139,6 +148,7 @@ def main():
     # Release camera and destroy windows
     camera.release()
     cv2.destroyAllWindows()
+
 
 if __name__ == "__main__":
     main()
